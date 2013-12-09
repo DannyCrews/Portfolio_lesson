3d6520bfa0f5c0e65ecd6a8ed72d042c527c8ebc
Theodore Cleveland <tcclevela@gmail.com>
2013-12-09 16:04:12 -0500

    adds javascript to make navigation bar work

--- main.js
+++ main.js
@@ -3,7 +3,19 @@ var Page = {
   setNavigationClicks: function() {
     $('.navigation-wrapper a').click(function(e) {
       e.preventDefault();
+      Page.scrollToSection($(e.currentTarget).attr('href'));
     });
+  },
+
+  scrollTo: function(yValue) {
+    $('html body').animate({
+      'scrollTop':yValue
+    }, 1000);
+  },
+
+  scrollToSection: function(sectionString) {
+    var sectionClass = "." + sectionString;
+    Page.scrollTo($(sectionClass).offset().top - 100);
   }
 
 };
