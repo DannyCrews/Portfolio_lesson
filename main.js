fdf09ccd051f958699aed505c2b92cd5af720302
Theodore Cleveland <tcclevela@gmail.com>
2013-12-09 15:09:36 -0500

    styling, adds foundation grid system and fixes foundation overrides on our css

---
+++ main.js
@@ -0,0 +1,13 @@
+var Page = {
+
+  setNavigationClicks: function() {
+    $('.navigation-wrapper a').click(function(e) {
+      e.preventDefault();
+    });
+  }
+
+};
+
+$(function() {
+  Page.setNavigationClicks();
+});
